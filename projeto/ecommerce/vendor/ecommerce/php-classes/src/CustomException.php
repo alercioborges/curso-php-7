ed49a9a95ec95d9915c5b887092ceff3679e087e
Alercio Silva <alercio.borges@corp.directweb.br>
2023-08-07 16:23:44 -0300

    07-08-2023

---
+++ projeto/ecommerce/vendor/ecommerce/php-classes/src/CustomException.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Ecommerce;
+
+class CustomException extends \Exception
+{
+	public function errorMessage() {
+    //error message
+		$errorMsg = 'Error on line '.$this->getLine().' in '.$this->getFile()
+		.': <b>'.$this->getMessage().'</b> is not a valid E-Mail address';
+		return $errorMsg;
+	}
+}
+
+?>
